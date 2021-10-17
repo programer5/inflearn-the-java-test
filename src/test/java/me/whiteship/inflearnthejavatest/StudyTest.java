5791f85c5984a439581f0d00eb7be3c8782283ac
minseo23 <neverdie4757@gmail.com>
2021-10-17 15:19:02 +0900

    JUnit5

---
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -0,0 +1,43 @@
+package me.whiteship.inflearnthejavatest;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class StudyTest {
+
+    @Test
+    @DisplayName("스터디 만들기 \uD83D\uDE31")
+    void create_new_study() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    @DisplayName("스터디 만들기 \uD83D\uDE31")
+    void create_new_study_again() {
+        System.out.println("create");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+}
