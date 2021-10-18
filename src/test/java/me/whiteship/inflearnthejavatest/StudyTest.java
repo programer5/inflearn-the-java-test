c4e1a1cf13ce907708001c5ee49f76c32a39a458
minseo23 <neverdie4757@gmail.com>
2021-10-18 23:56:16 +0900

    JUnit5 Assertion

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -2,6 +2,10 @@ package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 
+import java.lang.reflect.Executable;
+import java.time.Duration;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -10,10 +14,28 @@ class StudyTest {
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
     void create_new_study() {
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("create");
+
+        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
+            new Study(10);
+            Thread.sleep(300);
+        });
+        // TODO ThreadLocal
+
+//        assertTimeout(Duration.ofMillis(100), () -> {
+//            new Study(10);
+//            Thread.sleep(300);
+//        });
     }
+//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+//        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
+//        Study study = new Study(-10);
+
+//        assertAll(
+//                () -> assertNotNull(study),
+//                () ->assertEquals(StudyStatus.DRAFT, study.getStatus(),
+//                    () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
+//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
+//        );
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
