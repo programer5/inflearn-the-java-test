f478fd83c47699aa31a9de96fff0eaa0ac0fa6ed
minseo23 <neverdie4757@gmail.com>
2021-10-21 22:17:28 +0900

    JUnit5 조건에 따라 테스트 실행하기

--- src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/StudyTest.java
@@ -1,41 +1,31 @@
 package me.whiteship.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import java.lang.reflect.Executable;
 import java.time.Duration;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
+    @EnabledOnOs({OS.MAC, OS.WINDOWS})
     void create_new_study() {
+        String test_env = System.getenv("TEST_ENV");
+        System.out.println("test_env = " + test_env);
 
-        assertTimeoutPreemptively(Duration.ofMillis(100), () -> {
-            new Study(10);
-            Thread.sleep(300);
-        });
-        // TODO ThreadLocal
+        Study actual = new Study(10);
+        assertThat(actual.getLimit()).isGreaterThan(0);
 
-//        assertTimeout(Duration.ofMillis(100), () -> {
-//            new Study(10);
-//            Thread.sleep(300);
-//        });
     }
-//        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
-//        assertEquals("limit은 0보다 커야 한다.", exception.getMessage());
-//        Study study = new Study(-10);
-
-//        assertAll(
-//                () -> assertNotNull(study),
-//                () ->assertEquals(StudyStatus.DRAFT, study.getStatus(),
-//                    () -> "스터디를 처음 만들면 " + StudyStatus.DRAFT + " 상태다."),
-//                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.")
-//        );
 
     @Test
     @DisplayName("스터디 만들기 \uD83D\uDE31")
